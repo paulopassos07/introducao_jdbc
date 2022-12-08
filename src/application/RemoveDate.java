8709dd054de24000e1a35b52e800df54d448fa87
Paulo César Passos Pereira <paulo.passos.pereira1@outlook.com>
2022-12-08 09:46:21 -0300

    demo remove date

---
+++ src/application/RemoveDate.java
@@ -0,0 +1,40 @@
+package application;
+
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class RemoveDate {
+
+    public static void main(String[] args){
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "delete from departament"
+                        +"where"
+                        +"Id = ?"
+            );
+
+            st.setInt(1, 5);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        }
+        catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
