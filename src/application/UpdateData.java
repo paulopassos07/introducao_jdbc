6de7d4dfdcc258612732b6a050a163a18c089c1f
Paulo César Passos Pereira <paulo.passos.pereira1@outlook.com>
2022-12-08 08:57:11 -0300

    demo update data

---
+++ src/application/UpdateData.java
@@ -0,0 +1,39 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateData {
+    public static void main(String[] args){
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET BaseSalary = BaseSalary + ? "
+                            + "WHERE "
+                            + "(DepartmentId = ?)");
+
+            st.setDouble(1, 2000.0);
+            st.setInt(2, 1);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
